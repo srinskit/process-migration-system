8b4fd24c487937b62742693a7bfd99e73932137a
srinskit <srinskit@users.noreply.github.com>
2020-02-21 07:37:12 +0530

    Added testing automation script

---
+++ bench/run.sh
@@ -0,0 +1,27 @@
+killall -s9 pm-pro
+./pm-pro
+pids=($(pgrep pm-pro))
+pid1=${pids[0]}
+pid2=${pids[1]}
+
+sleep 2
+echo 'stopping' $pid2
+kill -STOP $pid2
+
+sleep 1
+echo 'stopping' $pid1
+kill -STOP $pid1
+
+echo "VMEM $pid1 >>> $pid2"
+sudo python ../vmem-copy/main.py $pid1 $pid2
+echo "VMEM copied"
+
+echo "KSTATE $pid1 >>> $pid2"
+echo "$pid1 $pid2" > /dev/regcp
+echo "KSTATE copied"
+
+kill -CONT $pid1
+kill -CONT $pid2
+wait
+
+diff child-*
