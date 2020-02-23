d4aaa8f8cb8a1b8e8cfd21415938809ca388bd10
Mahim Agrawal <mahim23agrawal@gmail.com>
2020-02-23 13:03:44 +0000

    Add client and server files

--- client.py
+++ client.py
@@ -1,31 +1,38 @@
 import socket
 import time
+import sys
+import re
 
+CHUNK_SIZE = 2048
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((sys.argv[3], 8220))
 
-client_socket.connect(("localhost", 8220))
+src_pid = sys.argv[1]
+src_maps_file = open("/proc/{}/maps".format(src_pid), 'r')
+src_mem_file = open("/proc/{}/mem".format(src_pid), 'rb')
 
-start = [ 2,4,1,3,42]
-chunks = ['chunk1','chunk2','chunk3','chunk4','chunk5',]
+client_socket.send(sys.argv[2].encode())
 
-index = 0
-while True:
-    data = str(start[index]) 
-    print('send to server: ' + data)
-    client_socket.send(data.encode())
-    while client_socket.recv(2048).decode().strip() != "ack":
-        print("waiting for ack")
-    data = chunks[index]
-    print('send to server: ' + data)
-    client_socket.send(data.encode())
-    print("ack received!")
-    if index == 4: #condition to stop sending data from client to server.
-        break
-    index = index + 1
+for line in src_maps_file.readlines():
+    m = re.match(r'([0-9A-Fa-f]+)-([0-9A-Fa-f]+) ([-r])', line)
+    # if line.find("vsyscall") != -1 or line.find("vdso") != -1 or line.find("vvar") != -1:
+    #     continue
+    if line.find("stack") == -1 and line.find("heap") == -1:
+        continue
+    if m.group(3) == 'r':
+        start = int(m.group(1), 16)
+        end = int(m.group(2), 16)
+        print(m.group(1), m.group(2))
+        for start_i in range(start, end, CHUNK_SIZE):
+            src_mem_file.seek(start_i)
+            chunk = src_mem_file.read(CHUNK_SIZE)
+            client_socket.send(str(start_i).encode())
+            client_socket.recv(2048)
+            client_socket.send(chunk)
+            client_socket.recv(2048)
+            print("Sent:", start_i, "->", len(chunk))
 
-#send disconnect message                                                                                                                           
-dmsg = "disconnect"
-print("Disconnecting")
-client_socket.send(dmsg.encode())
+client_socket.send("done".encode())
+client_socket.recv(2048)
 client_socket.close()
--- server.py
+++ server.py
@@ -1,31 +1,65 @@
 import socket
 import sys
+import subprocess
+import threading
+import signal
 
-host = 'localhost'
+
+CHUNK_SIZE = 2048
+BASE_PATH = "/home/ubuntu/mp-executables/"
+
+host = '0.0.0.0'
 port = 8220
 address = (host, port)
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(address)
-server_socket.listen(5)
+server_socket.listen(3)
 
-flag = 1 #for server to identify start or chunk. 1 = Start ; 0 = Chunk.
 
-print("Listening for client . . .")
-conn, address = server_socket.accept()
-print("Connected to client at ", address)
-while True:
-    output = conn.recv(2048)
-    if output == "disconnect".encode():
-        conn.close()
-        sys.exit("Received disconnect message.  Shutting down.")
-        conn.send("dack".encode())
-    elif output:
-        print("Message received from client:")
-        if flag == 1:
-            print("Start : " + output.decode())
-            flag = 0
+def on_new_client(client_socket, addr):
+    flag = 1
+    received_message = client_socket.recv(CHUNK_SIZE)
+
+    # p = subprocess.Popen([BASE_PATH + received_message.decode()])
+    # p.send_signal(signal.SIGSTOP)
+    # print("PID:", p.pid)
+
+    dst_pid = sys.argv[1]
+    dst_maps_file = open("/proc/{}/maps".format(dst_pid), 'r')
+    dst_mem_file = open("/proc/{}/mem".format(dst_pid), 'wb')
+    print(dst_maps_file.read())
+    start = ""
+    chunk = ""
+    while True:
+        received_message = client_socket.recv(CHUNK_SIZE)
+        if received_message != b'done':
+            # print("Message received from client:")
+            if flag == 1:
+                # print("Start : " + received_message.decode())
+                try:
+                    start = int(received_message.decode())
+                    flag = 0
+                except:
+                    print("Except:", received_message)
+                    dst_mem_file.close()
+            else:
+                # print("Chunk : " + received_message.decode())
+                chunk = received_message
+                dst_mem_file.seek(start)
+                bytes_written = dst_mem_file.write(chunk)
+                print(hex(start), bytes_written)
+                flag = 1
+            client_socket.send("ack".encode())
         else:
-            print("Chunk : " + output.decode())
-            flag = 1
-        conn.send("ack".encode())
+            print("Done received")
+            client_socket.send("ack".encode())
+            dst_mem_file.close()
+
+
+while True:
+    c, addr = server_socket.accept()
+    x = threading.Thread(target=on_new_client, args=(c, addr))
+    x.start()
+
+s.close()
