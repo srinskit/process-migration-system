d11386aa41e560b17f10b4d9bea4aa46ac30e31b
srinskit <srinskit@users.noreply.github.com>
2020-05-26 20:22:57 +0530

    Added recursive printer

---
+++ process/rec-printer.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+FILE *fp;
+
+void rec(int i)
+{
+	if (i > 40)
+	{
+		return;
+	}
+	fprintf(fp, "%d\n", i);
+	fflush(fp);
+	usleep(100000);
+	rec(i + 1);
+}
+
+int main()
+{
+	fp = fopen("op", "w");
+	rec(1);
+	fclose(fp);
+}
